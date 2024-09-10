ac7c4af5ce8b426934e0d34b5cb7f0b1191a536f
Satya br <satyabr147@gmail.com>
2024-09-10 08:38:02 +0700

    add IT-dev

---
+++ index.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Halaman sudah di-load");
+    getProductFromDummyJson();
+});
+
+function getProductFromDummyJson() {
+    fetch('https://dummyjson.com/products') // Mengambil data produk dari DummyJSON
+        .then(response => response.json())
+        .then(data => {
+            var container = document.querySelector('#product_container');
+            container.innerHTML = ''; // Kosongkan container sebelum menambahkan produk baru
+            // Batasi jumlah produk yang diambil hanya 10
+            const limitedProducts = data.products.slice(0, 10);
+
+            // Looping produk yang sudah dibatasi
+            limitedProducts.forEach(product => {
+                product.description = shortText(product.description, 70); // Batasi panjang deskripsi
+                product.title = shortText(product.title, 20); // Batasi panjang judul
+                container.insertAdjacentHTML('beforeend',
+                    `<div class="card">
+                        <div class="card-img">
+                            <img src="${product.thumbnail}" alt="img product">
+                        </div>
+                        <section>
+                            <header>
+                                <p class="fw-bold">${product.title}</p>
+                            </header>
+                            <p>${product.description}</p>
+                            
+                            <a href="detail/detail.html?id=${product.id}">
+                                <button class="bg-blue p-2 text-white fw-bold border-radius shadow">detail</button>
+                            </a>
+                        </section>
+                    </div>`
+                );
+            });
+        })
+        .catch(error => console.error('Error fetching products:', error));
+}
+
+// Fungsi untuk mempersingkat teks
+function shortText(text, maxLength) {
+    return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+}
